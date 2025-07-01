fc40ba1106b251a1e81ce32fa06d4855dc61029f
gcaltair <143710288+gcaltair@users.noreply.github.com>
2025-07-01 22:33:57 +0800

    添加空头文件

---
+++ Core/Inc/hc04.h
@@ -0,0 +1,8 @@
+//
+// Created by G on 25-7-1.
+//
+
+#ifndef FINAL_FINA_HC04_H
+#define FINAL_FINA_HC04_H
+
+#endif //FINAL_FINA_HC04_H
---
+++ Core/Src/hc04.c
@@ -0,0 +1,5 @@
+//
+// Created by G on 25-7-1.
+//
+
+#include "hc04.h"
